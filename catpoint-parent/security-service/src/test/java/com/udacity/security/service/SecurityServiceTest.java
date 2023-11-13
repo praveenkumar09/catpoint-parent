54be66114a1267b0fe0b3a3f89f36140b0184e3c
praveenkumar09 <tpkprav@gmail.com>
2023-11-13 18:51:05 +0800

    Fixed poms with property tags

---
+++ catpoint-parent/security-service/src/test/java/com/udacity/security/service/SecurityServiceTest.java
@@ -0,0 +1,52 @@
+package com.udacity.security.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecurityServiceTest {
+
+    @Test
+    void setArmingStatus() {
+    }
+
+    @Test
+    void addStatusListener() {
+    }
+
+    @Test
+    void removeStatusListener() {
+    }
+
+    @Test
+    void setAlarmStatus() {
+    }
+
+    @Test
+    void changeSensorActivationStatus() {
+    }
+
+    @Test
+    void processImage() {
+    }
+
+    @Test
+    void getAlarmStatus() {
+    }
+
+    @Test
+    void getSensors() {
+    }
+
+    @Test
+    void addSensor() {
+    }
+
+    @Test
+    void removeSensor() {
+    }
+
+    @Test
+    void getArmingStatus() {
+    }
+}

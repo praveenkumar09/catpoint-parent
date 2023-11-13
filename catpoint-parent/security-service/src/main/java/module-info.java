f3f84ca88bcb3dcad142af1b7d8ca35cfb229b77
praveenkumar09 <tpkprav@gmail.com>
2023-11-13 12:45:39 +0800

    Projects seperated to child projects with Java Modules --imageService & --securityService

---
+++ catpoint-parent/security-service/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module security.service {
+
+    requires image.service;
+    requires java.prefs;
+    requires com.google.gson;
+    requires com.google.common;
+    requires java.desktop;
+    requires com.miglayout.swing;
+}

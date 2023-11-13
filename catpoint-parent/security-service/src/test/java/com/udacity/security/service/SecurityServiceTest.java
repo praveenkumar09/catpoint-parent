1e3fa0f6f6d4fbac550210ef04749df78f2695da
praveenkumar09 <tpkprav@gmail.com>
2023-11-13 19:23:02 +0800

    Security Service Test(Armed Status Test) added

--- catpoint-parent/security-service/src/test/java/com/udacity/security/service/SecurityServiceTest.java
+++ catpoint-parent/security-service/src/test/java/com/udacity/security/service/SecurityServiceTest.java
@@ -1,13 +1,57 @@
 package com.udacity.security.service;
 
+import com.udacity.image.service.FakeImageService;
+import com.udacity.security.application.StatusListener;
+import com.udacity.security.data.AlarmStatus;
+import com.udacity.security.data.ArmingStatus;
+import com.udacity.security.data.SecurityRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+
+@ExtendWith(MockitoExtension.class)
 class SecurityServiceTest {
 
-    @Test
-    void setArmingStatus() {
+
+    private SecurityService securityService;
+
+    @Mock
+    private SecurityRepository securityRepository;
+
+    @Mock
+    private FakeImageService imageService;
+
+    @Mock
+    private StatusListener statusListener;
+
+    @BeforeEach
+    void setUp(){
+        securityService = new SecurityService(securityRepository,imageService);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ArmingStatus.class,names = {"ARMED_HOME","ARMED_AWAY"})
+    void setArmingStatusToArmed_changeStatusToArmed(ArmingStatus status) {
+        securityService.setArmingStatus(status);
+        verify(securityRepository,times(1)).setArmingStatus(status);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ArmingStatus.class,names = {"DISARMED"})
+    void setArmingStatusToDisarmed_changeStatusToNoAlarm(ArmingStatus status) {
+        securityService.setArmingStatus(status);
+        verify(securityRepository,times(1)).setAlarmStatus(AlarmStatus.NO_ALARM);
+        verify(securityRepository,times(1)).setArmingStatus(status);
     }
 
     @Test
